7010fa8b8af6aa3f3a258128fee7c7b7402c9837
Correl Roush <correl@gmail.com>
2010-12-08 17:04:26 -0500

    Made the dashboard index (repo list) require a login

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import permission_required
 from codereview.dashboard.models import Repository
 
+@permission_required('dashboard.browse')
 def index(request):
     """ List available repositories
     """
