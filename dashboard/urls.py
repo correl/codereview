a364b878b233b000a26ab1eedad1e14cf91c58a8
Correl Roush <correl@gmail.com>
2010-12-08 17:01:09 -0500

    Created a logout link

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from codereview.dashboard.models import Repository
 
 def index(request):
     """ List available repositories
     """
     repositories = Repository.objects.all()
-    return render_to_response('dashboard/index.html',
-            {'repositories': repositories})
+    data = RequestContext(request, {
+        'repositories': repositories,
+    })
+    return render_to_response('dashboard/index.html', data) 
