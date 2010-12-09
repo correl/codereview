9bf2ae4544a1a26b129dbd056422fbd66d4bd95a
Correl Roush <correl@gmail.com>
2010-12-09 00:10:05 -0500

    Simplified the review view/edit url

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'^(?P<review_id>\d+)/$', 'codereview.review.views.edit'),
     (r'^new/$', 'codereview.review.views.new'),
-    (r'^edit/(?P<review_id>\d+)/$', 'codereview.review.views.edit'),
 )
