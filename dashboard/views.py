9213ecbf544329ff4344eb819493fbd7e5a4deb3
Correl Roush <correl@gmail.com>
2010-12-14 17:55:30 -0500

    Added reviews to the dashboard index view

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import permission_required
 from codereview.dashboard.models import Repository
+from codereview.review.models import Review
 
 @permission_required('dashboard.browse')
 def index(request):
     """ List available repositories
     """
     repositories = Repository.objects.all()
+    reviews = Review.objects.all().order_by('-created')
     data = RequestContext(request, {
         'repositories': repositories,
+        'reviews': reviews,
     })
     return render_to_response('dashboard/index.html', data) 
