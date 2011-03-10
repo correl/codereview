c5aac250280627092d0b186c1c6285117c19e67a
Correl Roush <correl@gmail.com>
2011-03-09 20:34:33 -0500

    Serve static files in debug mode

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from codereview import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -22,3 +23,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+            settings.MEDIA_ROOT}),
+    )
